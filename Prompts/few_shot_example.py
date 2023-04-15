67ba01067cd9d9663fe0e9e5fd95267f924245b5
Jase Hackman <jase.hackman@zapier.com>
2023-04-15 00:25:00 -0500

    Prompt examples

---
+++ Prompts/few_shot_example.py
@@ -0,0 +1,117 @@
+from langchain import PromptTemplate, FewShotPromptTemplate
+
+
+# First, create the list of few shot examples.
+examples = [
+    {"word": "happy", "antonym": "sad"},
+    {"word": "tall", "antonym": "short"},
+]
+
+# Next, we specify the template to format the examples we have provided.
+# We use the `PromptTemplate` class for this.
+example_formatter_template = """
+Word: {word}
+Antonym: {antonym}\n
+"""
+example_prompt = PromptTemplate(
+    input_variables=["word", "antonym"],
+    template=example_formatter_template,
+)
+
+# Finally, we create the `FewShotPromptTemplate` object.
+few_shot_prompt = FewShotPromptTemplate(
+    # These are the examples we want to insert into the prompt.
+    examples=examples,
+    # This is how we want to format the examples when we insert them into the prompt.
+    example_prompt=example_prompt,
+    # The prefix is some text that goes before the examples in the prompt.
+    # Usually, this consists of intructions.
+    prefix="Give the antonym of every input",
+    # The suffix is some text that goes after the examples in the prompt.
+    # Usually, this is where the user input will go
+    suffix="Word: {input}\nAntonym:",
+    # The input variables are the variables that the overall prompt expects.
+    input_variables=["input"],
+    # The example_separator is the string we will use to join the prefix, examples, and suffix together with.
+    example_separator="\n\n",
+)
+
+# We can now generate a prompt using the `format` method.
+print(few_shot_prompt.format(input="big"))
+# -> Give the antonym of every input
+# -> 
+# -> Word: happy
+# -> Antonym: sad
+# ->
+# -> Word: tall
+# -> Antonym: short
+# ->
+# -> Word: big
+# -> Antonym:
+
+#-------------------------
+from langchain.prompts.example_selector import LengthBasedExampleSelector
+
+
+# These are a lot of examples of a pretend task of creating antonyms.
+examples = [
+    {"word": "happy", "antonym": "sad"},
+    {"word": "tall", "antonym": "short"},
+    {"word": "energetic", "antonym": "lethargic"},
+    {"word": "sunny", "antonym": "gloomy"},
+    {"word": "windy", "antonym": "calm"},
+]
+
+# We'll use the `LengthBasedExampleSelector` to select the examples.
+example_selector = LengthBasedExampleSelector(
+    # These are the examples is has available to choose from.
+    examples=examples, 
+    # This is the PromptTemplate being used to format the examples.
+    example_prompt=example_prompt, 
+    # This is the maximum length that the formatted examples should be.
+    # Length is measured by the get_text_length function below.
+    max_length=25,
+)
+
+# We can now use the `example_selector` to create a `FewShotPromptTemplate`.
+dynamic_prompt = FewShotPromptTemplate(
+    # We provide an ExampleSelector instead of examples.
+    example_selector=example_selector,
+    example_prompt=example_prompt,
+    prefix="Give the antonym of every input",
+    suffix="Word: {input}\nAntonym:",
+    input_variables=["input"],
+    example_separator="\n\n",
+)
+
+# We can now generate a prompt using the `format` method.
+print(dynamic_prompt.format(input="big"))
+# -> Give the antonym of every input
+# ->
+# -> Word: happy
+# -> Antonym: sad
+# ->
+# -> Word: tall
+# -> Antonym: short
+# ->
+# -> Word: energetic
+# -> Antonym: lethargic
+# ->
+# -> Word: sunny
+# -> Antonym: gloomy
+# ->
+# -> Word: windy
+# -> Antonym: calm
+# ->
+# -> Word: big
+# -> Antonym:
+
+long_string = "big and huge and massive and large and gigantic and tall and much much much much much bigger than everything else"
+print(dynamic_prompt.format(input=long_string))
+# -> Give the antonym of every input
+
+# -> Word: happy
+# -> Antonym: sad
+# ->
+# -> Word: big and huge and massive and large and gigantic and tall and much much much much much bigger than everything else
+# -> Antonym:
---
+++ Prompts/getting_started.py
@@ -0,0 +1,46 @@
+from langchain import PromptTemplate
+
+
+template = """
+I want you to act as a naming consultant for new companies.
+
+Here are some examples of good company names:
+
+- search engine, Google
+- social media, Facebook
+- video sharing, YouTube
+
+The name should be short, catchy and easy to remember.
+
+What is a good name for a company that makes {product}?
+"""
+
+prompt = PromptTemplate(
+    input_variables=["product"],
+    template=template,
+)
+
+
+# An example prompt with no input variables
+no_input_prompt = PromptTemplate(input_variables=[], template="Tell me a joke.")
+no_input_prompt.format()
+# -> "Tell me a joke."
+
+# An example prompt with one input variable
+one_input_prompt = PromptTemplate(input_variables=["adjective"], template="Tell me a {adjective} joke.")
+one_input_prompt.format(adjective="funny")
+# -> "Tell me a funny joke."
+
+# An example prompt with multiple input variables
+multiple_input_prompt = PromptTemplate(
+    input_variables=["adjective", "content"], 
+    template="Tell me a {adjective} joke about {content}."
+)
+multiple_input_prompt.format(adjective="funny", content="chickens")
+# -> "Tell me a funny joke about chickens."
+
+# You can load prompts from LangChainHub
+from langchain.prompts import load_prompt
+
+prompt = load_prompt("lc://prompts/conversation/prompt.json")
+prompt.format(history="", input="What is 1 + 1?")
